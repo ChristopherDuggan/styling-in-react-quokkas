2a88a4268ec219aa2dd50ecdd0a7ceb730218ce8
ChristopherDuggan <christopherduggan@gmail.com>
2024-10-17 10:51:31 -0400

    finished lesson!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
-// Later on, we'll import button components here
+import Button1 from './components/normal-css/Button.jsx'
+import Button2 from './components/inline-css/Button.jsx'
+import Button3 from './components/css-in-js/Button.jsx'
+import Button4 from './components/styled-components/Button.jsx'
+import Button5 from './components/css-modules/Button.jsx'
+import Button6 from './components/sass/Button.jsx'
 
 const App = () => {
   return (
     <div className="App">
-      {
-        // Add all example components:
         <Button1 buttonText="Example 1" />
-        //...
-      }
+        <Button2 buttonText="Example 2" />
+        <Button3 buttonText="Example 3" />
+        <Button4 buttonText="Example 4" />
+        <Button5 buttonText="Example 5" />
+        <Button6 buttonText="Example 6" />
     </div>
   );
 };
